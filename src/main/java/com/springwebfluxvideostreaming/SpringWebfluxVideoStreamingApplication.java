e17fff24d38811f1599af224b6170969617e78ca
a.ahmed <a.ahmed@fz-juelich.de>
2022-07-09 11:02:44 +0200

    Initial commit

---
+++ src/main/java/com/springwebfluxvideostreaming/SpringWebfluxVideoStreamingApplication.java
@@ -0,0 +1,41 @@
+package com.springwebfluxvideostreaming;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class SpringWebfluxVideoStreamingApplication {
+
+    @Autowired
+    private StreamingService streamingService;
+
+    @GetMapping(value = "video/{title}", produces = "video/mp4")
+    public Mono<Resource> getVideos(
+            @PathVariable String title, @RequestHeader("Range") String range){
+
+        System.out.println("how many bytes, it getting from the server: "+range);
+
+        return streamingService.getVideo(title);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxVideoStreamingApplication.class, args);
+    }
+
+}
+
+/**
+ * twilio
+ * Account SID: ACf3c4ab234fe24cdb63bd8ac851a072a4
+ * Auth Token: 5227be86f752a951395c375d47a0a84e
+ *
+ *
+ */
